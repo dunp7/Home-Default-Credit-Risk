77f5c35bbb5ccf4d17c67802fe6f5fe0058f3c1e
dunp7 <dam0709123@gmail.com>
2024-01-02 20:45:21 +0700

    modify

---
+++ Preprocess Data/preprocess_pipeline.py
@@ -0,0 +1,20 @@
+''' This is a py file with purpose is creating preprocessing pipeline for all tables'''
+
+def preprocess_app_train_test(df):
+    pass
+
+def preprocess_prev_app(df):
+    pass
+
+def preprocess_bureau(df):
+    pass
+
+def preprocess_bureau_bal(df):
+    pass
+
+def preprocess_installment_payment(df):
+    pass
+
+def preprocess_credit_card_bal(df):
+    pass
+
